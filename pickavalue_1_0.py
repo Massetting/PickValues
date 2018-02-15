f4f5735c7393b73ba1233379725d5c0cd7f77c01
Andrea Massetti <andrea.massetti@monash.edu>
2018-02-15 13:36:02 +1100

    shaping the extractbypoint function to be mutiprocessed (throu the creation of some wrapper functions) and creating a metadata info function to give the information being saved the proper column name (eg band and date)

--- pickavalue_1_0.py
+++ pickavalue_1_0.py
@@ -9,44 +9,60 @@ import gdal,ogr
 import os
 from multiprocessing import Pool
 
-location=r'E:\NSW\indices'#raster path location###
-shloc=r'E:\ArcWorkspace\nsw'#shape location#
-shas=['control1','control2']#name of the point shapefile without extension .shp It is used for logging###
+location=r'E:\WA'#raster path location###
+shloc=r'E:\WA\gis_2017'#shape location#
+shas=['cooke_point','controlcookepoints']#name of the point shapefile without extension .shp It is used for logging###
 
-def extractbypoint(shpID):          
-        shpfile=shpID + '.shp'
-        shp=os.path.join(shloc,shpfile)                                
-        newDir=os.path.join(location,shpID) ##output location
-        if not os.path.exists(newDir):
-            os.mkdir(newDir)                        
-        filez = [f for f in os.listdir(location) if f.endswith(".tif")]##select the tif files      
-        filename={}
-        for fil in filez:
-            calcthis=[]
-            filename[fil]=os.path.join(location,fil)
-            band=filename[fil]
-            src_ds=gdal.Open(band) 
-            gt=src_ds.GetGeoTransform()
-            rb=src_ds.GetRasterBand(1)
-            countpnt=0
-            ds=ogr.Open(shp)
-            layer=ds.GetLayer()            
-            for feature in layer:
-                countpnt=countpnt+1
-                geom = feature.GetGeometryRef()
-                mx,my=geom.GetX(), geom.GetY()  #coord in map units
+shpfile=shpID + '.shp'
+shp=os.path.join(shloc,shpfile)
+                                
+outputlocation=r"E:\WA\Validation_RSE_after_Reviews" ##output location
+                      
+filez = [f for f in os.listdir(location) if f.endswith(".tif")]##select the tif files      
+shpA=os.path.join(shloc,(shas[0] + '.shp'))
+shpB=os.path.join(shloc,(shas[1] + '.shp'))
+
+def get_metadata(fil):
+    """extracts information from the filename
+    """
+    band=fil[:]
+    date=fil[:]
+    return band, date        
+def extractbypoint(shp,fil,fileout="csv"):         
+    """
+    """
+    calcthis = [] #TODO: should I really use a list here???
+    band = os.path.join(location,fil)    
+    src_ds = gdal.Open(band) 
+    gt = src_ds.GetGeoTransform()
+    rb = src_ds.GetRasterBand(1)
+    countpnt = 0
+    ds = ogr.Open(shp)
+    layer = ds.GetLayer()            
+    for feature in layer:
+        countpnt = countpnt+1
+        geom = feature.GetGeometryRef()
+        mx,my = geom.GetX(), geom.GetY()  #coord in map units
             #Convert from map to pixel coordinates.
-            #Only works for geotransforms with no rotation.
-                px = int((mx - gt[0]) / gt[1]) #x pixel
-                py = int((my - gt[3]) / gt[5]) #y pixel
-                intval=rb.ReadAsArray(px,py,1,1)
-                calcthis.append(intval[0])#####<<<<<--------------------------------intval[0] is the pixel value
-            src_ds=None
-            rb=None###close the datasets
-        
-
+            
+        px = int((mx - gt[0]) / gt[1]) #x pixel
+        py = int((my - gt[3]) / gt[5]) #y pixel
+        intval = rb.ReadAsArray(px,py,1,1)
+        calcthis.append(intval[0])#####<<<<<--------------------------------intval[0] is the pixel value
+    src_ds = None
+    rb = None
+    ds = None
+    layer = None ###close the datasets
+        #TODO: export to file
+    if fileout=="csv":
+        pass
+def apply_to_file(fil):
+    """Just so that the function gets only one argument for multiprocessing
+    """
+    extractbypoint(shpA,fil)
+    extractbypoint(shpB,fil)
         ##NOTE if you want to use multiprocessing you should save calcthis to a file
 if __name__ == '__main__':
-    pool = Pool()       
-    pool.map(extractbypoint, shas)#Multiprocess of the iteration:    for i in shas: extractbypoint(i)###just works as a for loop 
+    pool = Pool(6)       
+    pool.map(apply_to_file, filez)#Multiprocess of the iteration:    for i in shas: extractbypoint(i)###just works as a for loop 
     quit()
